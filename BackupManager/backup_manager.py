f5b7bbf13c214d24db08689f28125feea76bbe70
Jasper Preston <jas95.19@gmail.com>
2022-10-15 11:21:55 +0530

    Added function definition

---
+++ BackupManager/backup_manager.py
@@ -0,0 +1,13 @@
+def check_directory(dir) -> 'set':
+    """Function to recursively check the files and folders present in the given directory and returns as set"""
+    pass
+
+
+def copy_object(src, dest, obj_name):
+    """Function to copy the object from source directory to destination directory"""
+    pass
+
+
+def estimate_size(dir):
+    """Estimate the size of the given directory"""
+    pass
